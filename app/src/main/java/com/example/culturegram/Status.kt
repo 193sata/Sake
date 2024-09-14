f8e0bc78861012c27edc23a7523fde9a3bbb30f4
sataikumi <sata19300@gmail.com>
2024-09-15 04:03:23 +0900

    バグ修正

--- app/src/main/java/com/example/culturegram/Status.kt
+++ app/src/main/java/com/example/culturegram/Status.kt
@@ -3,7 +3,7 @@ package com.example.culturegram
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
+//import android.util.Log
 import androidx.exifinterface.media.ExifInterface
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -49,7 +49,7 @@ class Status {
     @Composable
     fun AchievementPage(navController: NavHostController, context: Context) {
         val syuzoList = loadOrCreateCsv()
-        updateAchievementStatus(syuzoList, context)
+        updateAchievementStatus(syuzoList)
 
         val visitedCount = syuzoList.count { it.visited }
         val totalCount = syuzoList.size
@@ -196,13 +196,13 @@ class Status {
         var currentRating by remember { mutableStateOf(syuzo.evaluation) }  // 現在の評価
 
         // アプリ再起動時に評価を読み込む
-//        LaunchedEffect(Unit) {
-//            val syuzoList = loadOrCreateCsv()
-//            val savedSyuzo = syuzoList.find { it.id == syuzo.id }
-//            if (savedSyuzo != null) {
-//                currentRating = savedSyuzo.evaluation  // 保存された評価を読み込む
-//            }
-//        }
+        LaunchedEffect(Unit) {
+            val syuzoList = loadOrCreateCsv()  // CSVからデータを読み込む
+            val savedSyuzo = syuzoList.find { it.id == syuzo.id }  // 現在の酒蔵に対応するデータを探す
+            if (savedSyuzo != null) {
+                currentRating = savedSyuzo.evaluation  // 保存された評価を読み込む
+            }
+        }
 
         Scaffold(
             topBar = {
@@ -368,8 +368,12 @@ class Status {
     private fun loadOrCreateCsv(): List<Syuzo> {
         val filePath = "/storage/emulated/0/Android/data/com.example.culturegram/files/csv/syuzo.csv"
         val csvFile = File(filePath)
+        println("check csvFile")
+        println(csvFile)
 
         // ファイルが存在しない場合、作成する
+        println("ファイルが存在")
+        println(csvFile.exists())
         if (!csvFile.exists()) {
             createCsv(csvFile)
         }
@@ -377,6 +381,8 @@ class Status {
         val syuzoList = mutableListOf<Syuzo>()
         if (csvFile.exists()) {
             csvFile.forEachLine { line ->
+                println("line")
+                println(line)
                 val parts = line.split(",")
                 if (parts.size == 7) {  // ID, name, representativeSake, latitude, longitude, visited, clear, link, single, beer, fruit, wine, spirits, liquor, other
                     val id = parts[0].toIntOrNull() ?: 0
@@ -385,7 +391,10 @@ class Status {
                     val latitude = parts[3].toDoubleOrNull() ?: 0.0
                     val longitude = parts[4].toDoubleOrNull() ?: 0.0
                     val visited = parts[5].toIntOrNull() == 1
-                    val evaluation = parts[6].toIntOrNull() ?: 3
+                    println("parts[6] is ")
+                    println(parts[6]) // 下の行で設定した値(3)
+                    val evaluation = parts[6].trim().toIntOrNull() ?: 3
+//                    val evaluation = parts[6].toInt()
 //                    val clear = parts[6].toIntOrNull() ?: 0
 //                    val link = parts[7].toIntOrNull() ?: 0
 //                    val single = parts[8].toIntOrNull() ?: 0
@@ -408,7 +417,7 @@ class Status {
     }
 
     // 達成度を更新する関数
-    private fun updateAchievementStatus(syuzoList: List<Syuzo>, context: Context) {
+    private fun updateAchievementStatus(syuzoList: List<Syuzo>) {
         syuzoList.forEach { syuzo ->
 //            val imagePath = getSavedImagePath(context, syuzo.name)
             val imagePath = "/storage/emulated/0/Android/data/com.example.culturegram/files/Pictures/${syuzo.name}-0.jpg"
@@ -457,48 +466,48 @@ class Status {
         file.parentFile?.mkdirs()
         file.writeText(
             """
-        1,瑞鷹（株）川尻本蔵,瑞鷹,32.73743225,130.6823003,0, 3
-        2,瑞鷹（株）東肥蔵,東肥赤酒,32.74092219,130.6813227,0, 3
-        3,熊本ワインファーム（株）西里醸造所,菊鹿シャルドネ,33.07834405,130.7703336,0, 3
-        4,熊本クラフトビール（有）,熊本クラフトビール,32.84064767,130.7754777,0, 3
-        5,（株）ダイヤモンドブルーイング,Mulberry Beliner Wenisse,32.81263289,130.7692731,0, 3
-        6,サントリービール（株）九州熊本工場,ザ・プレミアム・モルツ,32.7467578,130.7914407,0, 3
-        7,通潤酒造（株）,蟬,32.68511123,130.9878935,0, 3
-        8,（株）福田酒造,甘夏サングリア,32.2854224,130.5623211,0, 3
-        9,亀萬酒造（資）,亀萬,32.42791937,130.9968777,0, 3
-        10,繊月酒造（株）,繊月,32.34128749,130.6055628,0, 3
-        11,（資）壽福酒造場,武者返し,32.20648147,130.7655083,0, 3
-        12,深野酒造（株）,彩葉,32.23540363,130.7483955,0, 3
-        13,（株）福田農場,山河,32.20343637,130.7611627,0, 3
-        14,（資）大和一酒造元,温泉焼酎夢,32.21917863,130.7327364,0, 3
-        15,松本酒造場,萬縁,32.23303241,130.8938903,0, 3
-        16,松の泉酒造（資）,松の泉,32.24187256,130.9052102,0, 3
-        17,（資）高田酒造場,あさぎりの花,32.24546583,130.8758723,0, 3
-        18,（株）堤酒造,奥球磨櫻,32.23584619,130.9257499,0, 3
-        19,高橋酒造（株）,白岳しろ,32.25818578,130.6910527,0, 3
-        20,（有）那須酒造場,球磨の泉,32.24749793,130.94737,0, 3
-        21,木下醸造所球磨リキュール製造所,文蔵,32.26144048,130.9427931,0, 3
-        22,木下醸造所,梅酒文蔵,32.26144048,130.9427931,0, 3
-        23,房の露（株）,蔵八,32.26527412,130.9412771,0, 3
-        24,抜群酒造（資）,ばつぐん,32.27659958,130.9430283,0, 3
-        25,（名）豊永酒造,豊永蔵,32.28089257,130.9851592,0, 3
-        26,（有）林酒造所,極楽,32.28586697,130.9845676,0, 3
-        27,六調子酒造（株）,特吟六調子,32.19775304,130.8189891,0, 3
-        28,常楽酒造（株）,秋の露,32.19509828,130.8465653,0, 3
-        29,（有）大石酒造場,大石,32.29688453,130.9880785,0, 3
-        30,（有）松下醸造場,最古蔵,32.2964037,130.9866099,0, 3
-        31,企業組合青空中央企画,絆（きずな）,32.97729618,130.4570883,0, 3
-        32,花の香り酒造（株）,純米大吟醸花の香桜花,33.07499777,130.5927503,0, 3
-        33,（名）天草酒造,天草,32.32296759,130.1919306,0, 3
-        34,アマクサソナービール合同会社,ホワイトパール,32.54289799,130.1789321,0, 3
-        35,千代の園酒造（株）,大吟醸千代の園EXCEL,33.01123691,130.6876344,0, 3
-        36,熊本ワインファーム（株）菊鹿醸造場,菊鹿シャドルネアンウデット,32.85059192,130.6947742,0, 3
-        37,（株）美少年,美少年純米酒清夜,32.99694444,130.8720977,0, 3
-        38,アパラボ,アソピエス,32.97501398,131.0409505,0, 3
-        39,山村酒造（名）,れいざん,32.81738572,131.1251811,0, 3
-        40,河津酒造（株）,七歩蛇,33.12135288,131.0670264,0, 3
-        41,室原（名）,和田志ら露,33.09606039,131.0694134,0, 3
-        42,どぶろく加工所,風の杜,33.11380409,131.0594832,0, 3
+        1,瑞鷹（株）川尻本蔵,瑞鷹,32.73743225,130.6823003,0,3
+        2,瑞鷹（株）東肥蔵,東肥赤酒,32.74092219,130.6813227,0,3
+        3,熊本ワインファーム（株）西里醸造所,菊鹿シャルドネ,33.07834405,130.7703336,0,3
+        4,熊本クラフトビール（有）,熊本クラフトビール,32.84064767,130.7754777,0,3
+        5,（株）ダイヤモンドブルーイング,Mulberry Beliner Wenisse,32.81263289,130.7692731,0,3
+        6,サントリービール（株）九州熊本工場,ザ・プレミアム・モルツ,32.7467578,130.7914407,0,3
+        7,通潤酒造（株）,蟬,32.68511123,130.9878935,0,3
+        8,（株）福田酒造,甘夏サングリア,32.2854224,130.5623211,0,3
+        9,亀萬酒造（資）,亀萬,32.42791937,130.9968777,0,3
+        10,繊月酒造（株）,繊月,32.34128749,130.6055628,0,3
+        11,（資）壽福酒造場,武者返し,32.20648147,130.7655083,0,3
+        12,深野酒造（株）,彩葉,32.23540363,130.7483955,0,3
+        13,（株）福田農場,山河,32.20343637,130.7611627,0,3
+        14,（資）大和一酒造元,温泉焼酎夢,32.21917863,130.7327364,0,3
+        15,松本酒造場,萬縁,32.23303241,130.8938903,0,3
+        16,松の泉酒造（資）,松の泉,32.24187256,130.9052102,0,3
+        17,（資）高田酒造場,あさぎりの花,32.24546583,130.8758723,0,3
+        18,（株）堤酒造,奥球磨櫻,32.23584619,130.9257499,0,3
+        19,高橋酒造（株）,白岳しろ,32.25818578,130.6910527,0,3
+        20,（有）那須酒造場,球磨の泉,32.24749793,130.94737,0,3
+        21,木下醸造所球磨リキュール製造所,文蔵,32.26144048,130.9427931,0,3
+        22,木下醸造所,梅酒文蔵,32.26144048,130.9427931,0,3
+        23,房の露（株）,蔵八,32.26527412,130.9412771,0,3
+        24,抜群酒造（資）,ばつぐん,32.27659958,130.9430283,0,3
+        25,（名）豊永酒造,豊永蔵,32.28089257,130.9851592,0,3
+        26,（有）林酒造所,極楽,32.28586697,130.9845676,0,3
+        27,六調子酒造（株）,特吟六調子,32.19775304,130.8189891,0,3
+        28,常楽酒造（株）,秋の露,32.19509828,130.8465653,0,3
+        29,（有）大石酒造場,大石,32.29688453,130.9880785,0,3
+        30,（有）松下醸造場,最古蔵,32.2964037,130.9866099,0,3
+        31,企業組合青空中央企画,絆（きずな）,32.97729618,130.4570883,0,3
+        32,花の香り酒造（株）,純米大吟醸花の香桜花,33.07499777,130.5927503,0,3
+        33,（名）天草酒造,天草,32.32296759,130.1919306,0,3
+        34,アマクサソナービール合同会社,ホワイトパール,32.54289799,130.1789321,0,3
+        35,千代の園酒造（株）,大吟醸千代の園EXCEL,33.01123691,130.6876344,0,3
+        36,熊本ワインファーム（株）菊鹿醸造場,菊鹿シャドルネアンウデット,32.85059192,130.6947742,0,3
+        37,（株）美少年,美少年純米酒清夜,32.99694444,130.8720977,0,3
+        38,アパラボ,アソピエス,32.97501398,131.0409505,0,3
+        39,山村酒造（名）,れいざん,32.81738572,131.1251811,0,3
+        40,河津酒造（株）,七歩蛇,33.12135288,131.0670264,0,3
+        41,室原（名）,和田志ら露,33.09606039,131.0694134,0,3
+        42,どぶろく加工所,風の杜,33.11380409,131.0594832,0,3
         """.trimIndent()
         )
     }
